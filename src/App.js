6069d1a27df4bfee3df8be6b763ae844c4f3c6d7
Ivy Astrix <ivy.astrix@gmail.com>
2024-01-11 19:03:33 -0800

    connecting a wallet is working!! note that i am aware projectId should go in something like .env when we have a proper backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import QuizCard from './components/QuizCard'; // Adjust the import path based on your file structure
-
+import ConnectButton from './components/ConnectButton'
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react';
 const App = () => {
+    const projectId = "9e38cf00b8a28f9bc26631004877ae0e"
+    const mainnet = {
+        chainId: 1,
+        name: 'Ethereum',
+        currency: 'ETH',
+        explorerUrl: 'https://etherscan.io',
+        rpcUrl: 'https://cloudflare-eth.com'
+    }
+    const metadata = {
+        name: 'My Website',
+        description: 'My Website description',
+        url: 'https://mywebsite.com',
+        icons: ['https://avatars.mywebsite.com/']
+    }
+    createWeb3Modal({
+        ethersConfig: defaultConfig({ metadata }),
+        chains: [mainnet],
+        projectId
+    })
     // Example question and options
     const question = "Mathgirl, is 42 a prime number?";
     const options = ["Yes", "No"];
@@ -16,6 +36,7 @@ const App = () => {
         <div className="App">
             <header className="App-header">
                 <h1>Mathgirl</h1>
+                <ConnectButton>Connect Wallet</ConnectButton>
             </header>
             <main>
                 <QuizCard
